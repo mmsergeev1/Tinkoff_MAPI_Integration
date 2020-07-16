4f28fbf944322233e325797cb63b47e056372410
m.m.sergeev <m.m.sergeev1@gmail.com>
2020-07-16 18:58:03 +0300

    extended docs, some refactoring

--- MAPI_EACQ.py
+++ MAPI_EACQ.py
@@ -15,10 +15,15 @@ def handle_exceptions(function):
                 attempt += 1
                 if attempt > 2:
                     logger.log_exception(Exception, exc_info=True)
+
+
 # Todo: use handler in functions that can catch not handled errors
 
 
-def get_token(request_dict, token_password):
+def get_token(request_dict: dict, token_password: str) -> str:
+    """Generates token for request signature
+    https://oplata.tinkoff.ru/develop/api/request-sign/
+    """
     request_dict["Password"] = token_password
     concatenated_token_string = ''
     for key in sorted(request_dict.keys()):
@@ -28,7 +33,8 @@ def get_token(request_dict, token_password):
     return token
 
 
-def send_request(request_dict, request_url):
+def send_request(request_dict: dict, request_url: str) -> tuple:
+    """Sending request using requests module"""
     request_json = json.dumps(request_dict)
 
     headers = {'Content-type': 'application/json',
@@ -48,12 +54,14 @@ class RequestError(Exception):
     pass
 
 
+# TODO expand classes of exceptions
+
+
 class PaymentStatusError(Exception):
     pass
 
 
 class EACQ:
-
     test_terminal_key = 'TinkoffBankTest'
     test_environment_url = 'https://rest-api-test.tinkoff.ru/v2/'
     prod_environment_url = 'https://securepay.tinkoff.ru/v2/'
@@ -63,27 +71,44 @@ class EACQ:
         self.terminal_key = None
         self.token_password = None
         self.receipt = None
-        self.used_url = None
+        self.used_payment_gate_url = None
         self.data = None
         self.description = None
         self.status = None
         self.pay_type = None
 
-    def set_status(self, new_status):
+    def set_internal_payment_status(self, new_status: str) -> None:
+        """Sets internal payment status in instance of class in your system
+        It can be used just not to request status via API
+        """
         self.status = new_status
 
-    def set_url(self, url=test_environment_url):
-        self.used_url = url
-        return self.used_url
+    def set_used_payment_gate_url(self, url: str = test_environment_url) -> None:
+        """Sets url of environment you used on Init"""
+        self.used_payment_gate_url = url
 
-    def set_terminal(self, new_terminal_key=test_terminal_key, new_token_password=test_terminal_token_password):
+    def get_used_payment_gate_url(self) -> str:
+        """Returns url of environment that was used"""
+        return self.used_payment_gate_url
+
+    def set_used_terminal_and_token_password(self, new_terminal_key: str = test_terminal_key,
+                                             new_token_password: str = test_terminal_token_password) -> None:
+        """Saves used on Init terminal key and token password"""
         self.terminal_key = new_terminal_key
         self.token_password = new_token_password
 
-    def get_status(self):
+    def get_used_terminal_and_token_password(self, need_token_password: bool = False):
+        """Returns used on Init terminal key and token password"""
+        if need_token_password:
+            return self.terminal_key, self.token_password
+        elif not need_token_password:
+            return self.terminal_key
+
+    def get_status(self) -> str:
+        """Returns internal payment status in your system"""
         return self.status
 
-    def set_description(self, value=f"Тестовый заказ от {getpass.getuser()}"):
+    def set_description(self, value=f"Тестовый заказ от {getpass.getuser()}") -> None:
         self.description = value
 
     def set_receipt(self, receipt_email='a@test.com', company_email='b@test.ru',
@@ -123,19 +148,27 @@ class EACQ:
             ]
         }  # TODO: make dynamic receipt
 
-    def set_data(self, data_phone="+71234567890",
-                 data_email=f"{getpass.getuser()}@tinkoff.ru"):
+    def set_data(self, data_phone: str = "+71234567890",
+                 data_email: str = f"{getpass.getuser()}@gmail.com") -> None:
+        """Sets extra payment data sent to payment gateway
+        https://oplata.tinkoff.ru/develop/api/payments/init-request/
+        """
         self.data = {
             "Phone": data_phone,
             "Email": data_email
         }
 
-    def init(self, order_id, terminal_key=test_terminal_key, token_password=test_terminal_token_password,
-             amount=100, url=test_environment_url, two_step=False, init_token_required=False, recurrent=False):
+    def init(self, order_id: str, terminal_key: str = test_terminal_key,
+             token_password: str = test_terminal_token_password, amount: int = 100, url: str = test_environment_url,
+             two_step:  bool = False, init_token_required: bool = False, recurrent: bool = False) -> tuple:
+        """Initialises payment session
+        https://oplata.tinkoff.ru/develop/api/payments/init-description/
+        """
+        self.set_used_terminal_and_token_password(terminal_key, token_password)
 
-        self.set_terminal(terminal_key, token_password)
+        self.set_used_payment_gate_url(url)
 
-        request_url = self.set_url(url) + 'Init'
+        request_url = self.get_used_payment_gate_url() + 'Init'
 
         if two_step:
             self.pay_type = 'T'
@@ -143,7 +176,7 @@ class EACQ:
             self.pay_type = 'O'
 
         request_dict = {
-            "TerminalKey": self.terminal_key,
+            "TerminalKey": self.get_used_terminal_and_token_password(),
             "Amount": amount,
             "OrderId": order_id,
             "PayType": self.pay_type
@@ -171,8 +204,11 @@ class EACQ:
 
         return answer_code, response
 
-    def get_state(self, payment_id):
-        request_url = self.used_url + 'GetState'
+    def get_state(self, payment_id: str) -> tuple:
+        """Gets payment status in acquiring system of bank
+        https://oplata.tinkoff.ru/develop/api/payments/getstate-description/
+        """
+        request_url = self.used_payment_gate_url + 'GetState'
 
         request_dict = {
             "TerminalKey": self.terminal_key,
@@ -184,13 +220,16 @@ class EACQ:
 
         response_list = send_request(request_dict, request_url)
 
-        logger.log_into_file(response_list[0], response_list[1], 'GetState',  level='Debug', request_dict=request_dict)
+        logger.log_into_file(response_list[0], response_list[1], 'GetState', level='Debug', request_dict=request_dict)
 
         return response_list
 
-    def confirm(self, payment_id):
+    def confirm(self, payment_id: str) -> tuple:
+        """Confirms payment and transaction
+        https://oplata.tinkoff.ru/develop/api/payments/confirm-description/
+        """
         if self.status not in ['CONFIRMED', 'None']:
-            request_url = self.used_url + 'Confirm'
+            request_url = self.used_payment_gate_url + 'Confirm'
 
             request_dict = {
                 "TerminalKey": self.terminal_key,
@@ -202,16 +241,19 @@ class EACQ:
 
             response_list = send_request(request_dict, request_url)
 
-            logger.log_into_file(response_list[0], response_list[1], 'Confirm',  level='Debug',
+            logger.log_into_file(response_list[0], response_list[1], 'Confirm', level='Debug',
                                  request_dict=request_dict)
 
             return response_list
         else:
             raise PaymentStatusError(f"Status is not valid for Confirm. Status: {self.status}.")
 
-    def cancel(self, payment_id, cancel_amount=0, full_cancel=True):
+    def cancel(self, payment_id: str, cancel_amount: int = 0, full_cancel: bool = True) -> tuple:
+        """Cancels payment and refunds to card
+        https://oplata.tinkoff.ru/develop/api/payments/cancel-description/
+        """
         if self.status in ['NEW', 'AUTHORIZED', 'CONFIRMED']:
-            request_url = self.used_url + 'Cancel'
+            request_url = self.used_payment_gate_url + 'Cancel'
 
             request_dict = {
                 "TerminalKey": self.terminal_key,
@@ -230,7 +272,7 @@ class EACQ:
 
             response_list = send_request(request_dict, request_url)
 
-            logger.log_into_file(response_list[0], response_list[1], 'Cancel',  level='Debug',
+            logger.log_into_file(response_list[0], response_list[1], 'Cancel', level='Debug',
                                  request_dict=request_dict)
 
             return response_list
@@ -238,9 +280,12 @@ class EACQ:
         else:
             raise PaymentStatusError(f"Status is not valid for Cancel. Status: {self.status}.")
 
-    def charge(self, payment_id, rebill_id):
+    def charge(self, payment_id: str, rebill_id: str) -> tuple:
+        """Makes recurrent payment from pre-saved card
+        https://oplata.tinkoff.ru/develop/api/autopayments/charge-description/
+        """
         if self.status in ['NEW']:
-            request_url = self.used_url + 'Charge'
+            request_url = self.used_payment_gate_url + 'Charge'
 
             request_dict = {
                 "TerminalKey": self.terminal_key,
@@ -253,7 +298,7 @@ class EACQ:
 
             response_list = send_request(request_dict, request_url)
 
-            logger.log_into_file(response_list[0], response_list[1], 'Charge',  level='Debug',
+            logger.log_into_file(response_list[0], response_list[1], 'Charge', level='Debug',
                                  request_dict=request_dict)
 
             return response_list
--- process.py
+++ process.py
@@ -8,12 +8,17 @@ import webbrowser
 eacq = MAPI_EACQ.EACQ()
 
 
-def gen_order_id(order_id_length=8):
+def gen_order_id(order_id_length: int = 8) -> str:
+    """Returns random string of len=order_id_length.
+    It can be used as order id"""
     letters = string.ascii_letters + '1234567890'
     return ''.join(random.choice(letters) for i in range(order_id_length))
 
 
-def send_eacq_init():
+def send_eacq_init() -> tuple:
+    """Sends Init and handles possible answers and exceptions.
+    Can be used as example of realization only.
+    """
     order_id = gen_order_id()
 
     try:
@@ -21,13 +26,13 @@ def send_eacq_init():
         answer_code, init_response = eacq.init(order_id, two_step=True)
 
         payment_url = init_response["PaymentURL"]
-        eacq.set_status(init_response["Status"])
+        eacq.set_internal_payment_status(init_response["Status"])
         payment_id = init_response["PaymentId"]
         webbrowser.open_new(payment_url)
         time.sleep(20)
 
         get_state_response = eacq.get_state(payment_id)
-        eacq.set_status(get_state_response[1]["Status"])
+        eacq.set_internal_payment_status(get_state_response[1]["Status"])
 
         return payment_id, get_state_response
 
@@ -42,12 +47,19 @@ def send_eacq_init():
         logger.log_exception(MAPI_EACQ.PaymentStatusError, exc_info=True)
 
 
-def send_eacq_confirm(payment_id):
+def send_eacq_confirm(payment_id: str) -> dict:
+    """
+    Sends Confirm and handles possible answers and exceptions.
+    Can be used as example of realization only.
+
+    :param payment_id: str
+    :return: payment gateway response dictionary
+    """
 
     try:
 
         answer_code, confirm_response = eacq.confirm(payment_id)
-        eacq.set_status(confirm_response["Status"])
+        eacq.set_internal_payment_status(confirm_response["Status"])
 
         return confirm_response
 
@@ -62,11 +74,18 @@ def send_eacq_confirm(payment_id):
         logger.log_exception(MAPI_EACQ.PaymentStatusError, exc_info=True)
 
 
-def send_eacq_cancel(payment_id):
+def send_eacq_cancel(payment_id: str) -> dict:
+    """
+    Sends Cancel and handles possible answers and exceptions.
+    Can be used as example of realization only.
+
+    :param payment_id: str
+    :return: payment gateway response dictionary
+    """
     try:
 
         answer_code, cancel_response = eacq.cancel(payment_id, full_cancel=True)
-        eacq.set_status(cancel_response["Status"])
+        eacq.set_internal_payment_status(cancel_response["Status"])
 
         return cancel_response
 
